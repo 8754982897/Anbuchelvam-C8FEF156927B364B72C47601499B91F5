a8f929d68fd9de4493889fa80bfa05e60d706c83
8754982897 <144230019+8754982897@users.noreply.github.com>
2023-09-07 17:37:20 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+#Implement a recursive function to calculate the factorial#
+def fact_rec(n):
+  if n == 0 or n == 1:
+    return 1
+  else:
+    return n * fact_rec(n - 1)
+
+
+number = int(input("Enter a value:"))
+res = fact_rec(number)
+print("The factorial of {} is {}.".format(number, res))
